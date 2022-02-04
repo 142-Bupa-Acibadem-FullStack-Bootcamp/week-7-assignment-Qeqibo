2797a733e6654108c27bc3dec741580f222dac65
Qeqibo <tarhanubeyt@gmail.com>
2022-02-04 17:49:43 +0300

    .

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  //title = 'CurrencyExchange';
+  currencies = ['TRY', 'EUR', 'USD'];
+  inCurr = "TRY";
+  outCurr = "EUR";
+  inrToForeignRates = {
+    TRY: 1,
+    EUR: 0.064,
+    USD: 0.074
+  };
+  inputValue = 10;
+  
+  }; 
+
+  fY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøífY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøífY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøí
+    fY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøí
+      fY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøí
+
+      fY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøí
+  fY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøí
+    fY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøífY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøífY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøí
+      fY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøívfY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøí
+        fY—nßødAá;{Yà´Ö­·\Y-´ŠßwvåÜßøí
+
